5d79a300d468e6024c77e67244ee68a4a631cb97
Adil <abbasov.adil.n@gmail.com>
2024-02-23 15:01:34 +0400

    updated

---
+++ src/AuthenticationWithWebAuthn/Controllers/WebAuthnController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationWithWebAuthn.Controllers
+{
+    [ApiController]
+    [Route("authn")]
+    public class WebAuthnController : ControllerBase
+    {
+
+
+    }
+}
